76d8dbbabdb9d3752b59b61aa535e0d7d8e10510
Patrik Albert <patrik.albert5@gmail.com>
2024-01-15 14:51:23 +0100

    prepare cron function for data retrieve

--- weather/main.py
+++ weather/main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+from time import sleep
 
 import uvicorn as uvicorn
 from fastapi import FastAPI
 import httpx
+from fastapi_restful.tasks import repeat_every
 
 app = FastAPI()
 
@@ -13,10 +15,23 @@ def hello():
 
 
 @app.get("/weather")
-def get_weather():
+def get_weather(q: str = "Kosice", units: str = 'metric', lang: str = "en"):
     print("Get weather")
-    response = httpx.get("https://api.openweathermap.org/data/2.5/weather?q=kosice&appid=9e547051a2a00f2bf3e17a160063002d")
+    params = {
+        "q": q,
+        "units": units,
+        "appid": "9e547051a2a00f2bf3e17a160063002d",
+        "lang": lang
+    }
+    response = httpx.get(
+        f"https://api.openweathermap.org/data/2.5/weather", params=params)
     return response.json()
+
+
+@app.on_event("startup")
+@repeat_every(seconds=10)
+def retrieve_weather_data():
+    print("retrieving weather data...")
 
 
 def main():
