af589a017fae756cbdb70b2492cad4d44823ae73
Patrik Albert <patrik.albert5@gmail.com>
2024-01-15 15:13:26 +0100

    retrieve weather data every 20 mins

--- weather/main.py
+++ weather/main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import json
 from time import sleep
 
 import uvicorn as uvicorn
@@ -15,24 +16,28 @@ def hello():
 
 
 @app.get("/weather")
-def get_weather(q: str = "Kosice", units: str = 'metric', lang: str = "en"):
-    print("Get weather")
+def get_weather():
+    with open("weather_data.json", "r") as data_f:
+        return json.load(data_f)
+
+
+@app.on_event("startup")
+@repeat_every(seconds=1200)
+def retrieve_weather_data(q: str = "Kosice", units: str = 'metric', lang: str = "en"):
+    print("retrieving weather data...")
+
     params = {
         "q": q,
         "units": units,
         "appid": "9e547051a2a00f2bf3e17a160063002d",
         "lang": lang
     }
+
     response = httpx.get(
         f"https://api.openweathermap.org/data/2.5/weather", params=params)
-    return response.json()
 
-
-@app.on_event("startup")
-@repeat_every(seconds=10)
-def retrieve_weather_data():
-    print("retrieving weather data...")
-
+    with open("weather_data.json", 'w') as data_f:
+        json.dump(response.json(), data_f, indent=2)
 
 def main():
     uvicorn.run('weather.main:app', reload=True,
