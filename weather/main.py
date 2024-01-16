9558f329eff1a4232d6873dd4eb000e570ad9ef5
Patrik Albert <patrik.albert5@gmail.com>
2024-01-16 09:55:48 +0100

    add logging

--- weather/main.py
+++ weather/main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+import http
 import json
 from time import sleep
+
+from loguru import logger
 
 import uvicorn as uvicorn
 from fastapi import FastAPI
@@ -17,6 +20,7 @@ def hello():
 
 @app.get("/weather")
 def get_weather():
+    logger.info("Getting weather")
     with open("weather_data.json", "r") as data_f:
         return json.load(data_f)
 
@@ -24,7 +28,7 @@ def get_weather():
 @app.on_event("startup")
 @repeat_every(seconds=1200)
 def retrieve_weather_data(q: str = "Kosice", units: str = 'metric', lang: str = "en"):
-    print("retrieving weather data...")
+    logger.info("Retrieving weather data.")
 
     params = {
         "q": q,
@@ -34,10 +38,14 @@ def retrieve_weather_data(q: str = "Kosice", units: str = 'metric', lang: str = 
     }
 
     response = httpx.get(
-        f"https://api.openweathermap.org/data/2.5/weather", params=params)
+        "https://api.openweathermap.org/data/2.5/weatherd", params=params)
 
-    with open("weather_data.json", 'w') as data_f:
-        json.dump(response.json(), data_f, indent=2)
+    if response.status_code == http.HTTPStatus.OK:
+        with open("weather_data.json", 'w') as data_f:
+            json.dump(response.json(), data_f, indent=2)
+    else:
+        logger.error(f"Data were not retrieved correctly. Status code: {response.status_code}")
+
 
 def main():
     uvicorn.run('weather.main:app', reload=True,
