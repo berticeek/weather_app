4e9582bd63c9da6f0776ff6232699c0f384eac43
Patrik Albert <patrik.albert5@gmail.com>
2024-01-16 10:31:27 +0100

    weather data model

---
+++ weather/models.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+import datetime
+
+
+class Measurement(BaseModel):
+    temperature: float      # °C
+    humidity: float
+    pressure: float         # hPa
+    city: str
+    country: str
+    wind_speed: float       # m/s
+    wind_direction: int     # °
+    dt: datetime            # UTC
+    sunrise: datetime       # UTC
+    sunset: datetime        # UTC
+    icon: str
