257bf590598de8e7cd27f6ff39d407b00046fe80
Patrik Albert <patrik.albert5@gmail.com>
2024-01-17 13:00:19 +0100

    dependencies module

---
+++ weather/dependencies.py
@@ -0,0 +1,9 @@
+from loguru import logger
+
+from weather.models.settings import Settings
+
+
+def get_settings():
+    settings = Settings()
+    logger.info(f"Loading settings for {settings.environment} environment")
+    return settings
