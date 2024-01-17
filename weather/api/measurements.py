8663d59b51b3468ae8dfda869786f317a1fdcea9
Patrik Albert <patrik.albert5@gmail.com>
2024-01-17 13:33:48 +0100

    dependency injection

--- weather/api/measurements.py
+++ weather/api/measurements.py
@@ -1,7 +1,7 @@
 from sqlmodel import create_engine, Session, select, desc
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
-from weather.dependencies import get_settings
+from weather.dependencies import get_settings, get_session
 from weather.models.measurement import Measurement
 from weather.models.settings import Settings
 
@@ -9,26 +9,14 @@ router = APIRouter()
 
 
 @router.get("/api/measurements")
-def list_measurements(city: str = None):
+def list_measurements(session: Session = Depends(get_session)):
     """Get all weather measurement data for all cities, or select by city"""
-
-    engine = create_engine(get_settings().db_uri)
-
-    with Session(engine) as session:
-        statement = select(Measurement)
-        if city is not None:
-            statement = statement.where(Measurement.city == city)
-        return session.exec(statement).all()
+    statement = select(Measurement)
+    return session.exec(statement).all()
 
 
 @router.get("/api/measurements/last")
-def get_last_measurement(city: str = None):
+def get_last_measurement(session: Session = Depends(get_session)):
     """Get last weather entry"""
-
-    engine = create_engine(get_settings().db_uri)
-
-    with Session(engine) as session:
-        statement = select(Measurement)
-        if city is not None:
-            statement = statement.where(Measurement.city == city)
-        return session.exec(statement.order_by(desc(Measurement.id))).first()
+    statement = select(Measurement)
+    return session.exec(statement.order_by(desc(Measurement.id))).first()
--- weather/dependencies.py
+++ weather/dependencies.py
@@ -1,6 +1,8 @@
 from functools import lru_cache
 
 from loguru import logger
+from sqlalchemy import create_engine
+from sqlmodel import Session
 
 from weather.models.settings import Settings
 
@@ -10,3 +12,9 @@ def get_settings() -> Settings:
     settings = Settings()
     logger.info(f"Loading settings for {settings.environment} environment")
     return settings
+
+
+def get_session():
+    engine = create_engine(get_settings().db_uri)
+    with Session(engine) as session:
+        yield session
