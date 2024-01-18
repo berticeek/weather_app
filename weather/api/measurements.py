d4dc12115f7fb7173841f2223d7116bd135472f8
Patrik Albert <patrik.albert5@gmail.com>
2024-01-18 09:37:10 +0100

    endpoint to get entry by id

--- weather/api/measurements.py
+++ weather/api/measurements.py
@@ -1,5 +1,8 @@
+import http
+
 from fastapi_pagination.ext.sqlmodel import paginate
 from fastapi_pagination.links import Page
+from sqlalchemy.exc import NoResultFound
 from sqlmodel import Session, select, desc
 from fastapi import APIRouter, Depends
 
@@ -7,7 +10,6 @@ from weather.dependencies import get_session
 from weather.models.measurement import Measurement
 
 router = APIRouter()
-# add_pagination(router)
 
 
 @router.get("/api/measurements", response_model=Page[Measurement])
@@ -22,3 +24,13 @@ def get_last_measurement(session: Session = Depends(get_session)):
     """Get last weather entry"""
     statement = select(Measurement)
     return session.exec(statement.order_by(desc(Measurement.id))).first()
+
+
+@router.get("/api/measurements/{slug}")
+def get_measurement_by_slug(slug: int, session: Session = Depends(get_session)):
+    """Get weather entry by its id"""
+    statement = select(Measurement)
+    try:
+        return session.exec(statement.where(Measurement.id == slug)).one()
+    except NoResultFound as e:
+        return {"error": f"Measurement with id {slug} doesn't exist"}
