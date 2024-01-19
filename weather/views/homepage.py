ed0afe4869d050a1be27b158acc179c2a01b2895
Patrik Albert <patrik.albert5@gmail.com>
2024-01-19 10:35:43 +0100

    bg image change based on current hour

--- weather/views/homepage.py
+++ weather/views/homepage.py
@@ -9,12 +9,14 @@ from sqlmodel import Session, select, desc
 from weather.api.measurements import get_last_measurement
 from weather.dependencies import get_templates, get_settings, get_session
 from weather.models.measurement import Measurement
+from weather.models.settings import Settings
 
 router = APIRouter()
 
 
 @router.get("/")
 def homepage(request: Request,
+             settings: Settings = Depends(get_settings),
              session: Session = Depends(get_session),
              templates: Jinja2Templates = Depends(get_templates)):
 
@@ -25,9 +27,13 @@ def homepage(request: Request,
         "request": request,
         "message": "Hello world.",
         "now": pendulum.now(),
-        "sunrise": pendulum.now(),
-        "sunset": pendulum.now(),
-        "weather": weather
+        "sunrise": weather.sunrise,
+        "sunset": weather.sunset,
+        "weather": weather,
+        "refresh": "45",
+        "version": "2024.01",
+        "environment": settings.environment,
+        "background_nr": pendulum.now().hour // 2 + 1
     }
 
     return templates.TemplateResponse("current.weather.html", context=context)
