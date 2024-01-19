bcc96b28a1b2ef5dc6bb4e82eb01abe0634860f6
Patrik Albert <patrik.albert5@gmail.com>
2024-01-19 11:46:49 +0100

    health check

--- weather/api/healthcheck.py
+++ weather/api/healthcheck.py
@@ -1,10 +1,34 @@
+import http
+
 from fastapi import APIRouter
+from fastapi.responses import JSONResponse
 
 router = APIRouter()
 
 
+def is_disk_healthy():
+    return True
+
+
+def is_db_healthy():
+    return True
+
+
 @router.get("/health")
 def healthcheck():
-    return {
-        "status": "ok"
-    }
+    disk = is_disk_healthy()
+    db = is_db_healthy()
+
+    if disk and db:
+        status = http.HTTPStatus.OK
+    else:
+        status = http.HTTPStatus.INTERNAL_SERVER_ERROR
+
+    return JSONResponse(
+        status_code=200,
+        content={
+            "status": status,
+            "disk": disk,
+            "database": db,
+        }
+    )
